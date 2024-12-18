315192268788ffd735bc173cd213c8e57ae65e10
MithilaKhan <mithilakhan082@gmail.com>
2024-12-18 12:08:43 +0600

    complete

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,20 @@ import ReduxProvider from "./redux/lib/ReduxProvider.tsx";
 import { InspectionProvider } from "./context/InspectionContext.tsx";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./Translation/i18n.tsx";
+import PrivateRoute from "./routes/PrivateRoute.tsx";
 
 
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>  
+  <StrictMode>   
+    <PrivateRoute>
     <ReduxProvider > 
  <InspectionProvider >  
  <I18nextProvider i18n={i18n}>
     <RouterProvider router={router} /> 
     </I18nextProvider>
  </InspectionProvider>
-    </ReduxProvider>
+    </ReduxProvider> 
+    </PrivateRoute>
   </StrictMode>
 );
---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,28 @@
+
+import { Navigate, useLocation } from "react-router-dom";
+import { useGetProfileQuery } from "../redux/features/auth/authApi";
+
+
+const PrivateRoute = ({ children }:{children: React.ReactNode}) => {
+  const location = useLocation();
+  const { data:profile, isLoading, isFetching, isError } = useGetProfileQuery(undefined); 
+  
+
+
+  if (isLoading || isFetching) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError || !profile?.data) {
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
+
+  if (profile?.data?.role === "ADMIN" || profile?.data?.role === "SUPERADMIN") {
+    return children; 
+  }
+
+  
+  return <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
